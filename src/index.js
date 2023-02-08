eabc1c2a0faa8216caf88b924821e2b132a0c542
Alexander <alex.galev@yahoo.com>
2023-02-08 11:15:41 -0700

    added clarifying comments

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
+// initializing pointers to DOM elements
 const nav = document.querySelector('#duck-nav')
 const name = document.querySelector('#duck-display-name')
 const image = document.querySelector('#duck-display-image')
 const likes = document.querySelector('#duck-display-likes')
 const form = document.querySelector('#new-duck-form')
 
+// initializing state
 let currentDuck = {}
 
+// adding event listeners
 likes.addEventListener('click', (e) => addLikes(e))
 form.addEventListener('submit', (e) => submitDuck(e))
 
+// initial fetch to get all ducks
 fetch('http://localhost:3000/ducks')
 	.then((res) => res.json())
 	.then((ducks) =>
@@ -21,18 +25,21 @@ fetch('http://localhost:3000/ducks')
 		})
 	)
 
+// render targeted duck upon click from nav
 const renderDuck = (duck) => {
 	currentDuck = duck
 	console.log(currentDuck)
 	name.textContent = duck.name
 	image.src = duck.img_url
-	likes.textContent = duck.likes
+	likes.textContent = `${duck.likes} Likes`
 }
 
+// add likes to duck object
+//fetch a patch request to update the database
+//update DOM
 const addLikes = (e) => {
 	currentDuck.likes++
 	console.log(currentDuck.likes)
-	// likes.textContent = currentDuck.likes
 	fetch(`http://localhost:3000/ducks/${currentDuck.id}`, {
 		method: 'PATCH',
 		headers: {
@@ -42,10 +49,13 @@ const addLikes = (e) => {
 	})
 		.then((res) => res.json())
 		.then((duck) => {
-			likes.textContent = duck.likes
+			likes.textContent = `${duck.likes} Likes`
 		})
 }
 
+// create new duck object
+//fetch a post request to update the database
+//update DOM
 const submitDuck = (e) => {
 	e.preventDefault()
 	const name = e.target['duck-name-input'].value
